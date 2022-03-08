78e33a2566830b6ba41cfebd8714a6e5cfc7596d
Ndanema98 <davidnda26@yahoo.co.uk>
2022-03-08 19:03:17 +0000

    Feat: Make questions appear at random once start button is clicked

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,10 @@
 const startButton = document.getElementById("start-btn")
 const questionContainerElements = document.getElementById("question-container") 
+const questionElement = document.getElementById("question")
+const answerButtonsElement = document.getElementById("answer-btns")
 const instructionContainerElements = document.getElementById("instructions")
+
+let shuffledQuestions, currentQuestionIndex
 
 startButton.addEventListener("click", startGame)
 function startGame() {
@@ -8,11 +12,23 @@ function startGame() {
     startButton.classList.add("hide")
     questionContainerElements.classList.remove("hide")
     instructionContainerElements.classList.add("hide")
-
+    shuffledQuestions = questions.sort(() => Math.random() - .5)
+    currentQuestionIndex = 0
+    nextQuestion()
 }
-function nextQuestion() {
-
+function nextQuestion(){
+    showQuestion(shuffledQuestions[currentQuestionIndex])
 }
-function selectAnswer(){
-
+function showQuestion(question){
+    questionElement.innerText = question.question
 }
+function selectAnswer(){}
+const questions = [
+    {
+        question: "What is the capital of New Zealand?"
+        answers: [ 
+            {text: "Wellington", correct: true}
+            {text: "London", correct: false}
+        ]
+    }
+]
