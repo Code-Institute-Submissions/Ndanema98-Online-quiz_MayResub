c96e190b614739269c47f56f8c21179c7d9a2efb
Ndanema98 <davidnda26@yahoo.co.uk>
2022-03-09 15:33:01 +0000

    Feat: Add questions for quiz

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,21 +4,15 @@ const questionContainerElements = document.getElementById("question-container")
 const questionElement = document.getElementById("question")
 const answerButtonsElement = document.getElementById("answer-btns")
 const instructionContainerElements = document.getElementById("instructions")
-const questions = [
-    {
-        question: "What is the capital of New Zealand?",
-        answers: [ 
-            {text: "Wellington", correct: true},
-            {text: "London", correct: false},
-            {text: "Paris", correct:false},
-            {text: "Rome", correct:false},
-        ]
-    }
-]
 
 let shuffledQuestions, currentQuestionIndex
 
 startButton.addEventListener("click", startGame)
+nextButton.addEventListener("click",() => {
+    currentQuestionIndex++
+    nextQuestion()
+})
+
 
 function startGame() {
     console.log("Started")
@@ -48,6 +42,7 @@ function showQuestion(question){
 }
 
 function resetState(){
+    
     nextButton.classList.add("hide")
     while (answerButtonsElement.firstChild){
         answerButtonsElement.removeChild
@@ -61,6 +56,11 @@ function selectAnswer(e){
      Array.from(answerButtonsElement.children).forEach(button => {
          setStatusClass(button, button.dataset.correct)
      })
+     if (shuffledQuestions.length > currentQuestionIndex + 1) {
+     nextButton.classList.remove("hide")
+} else {
+    startButton.innerText = "restart"
+    startButton.classList.remove("hide")
 }
 function setStatusClass(element, correct) {
     clearStatusClass(element)
@@ -74,4 +74,186 @@ function clearStatusClass(element) {
     element.classList.remove("correct")
     element.classList.remove("wrong")
 }
- 
+}
+const questions = [
+    {
+        question: "What is the capital of New Zealand?",
+        answers: [ 
+            {text: "Wellington", correct: true},
+            {text: "London", correct: false},
+            {text: "Paris", correct:false},
+            {text: "Rome", correct:false},
+        ]
+    },
+    {
+        question: "What does the Latin Tempus mean in English",
+        answers: [ 
+            {text: "Temperature", correct: false},
+            {text: "Tempest", correct: false},
+            {text: "Time", correct: true},
+            {text: "Tomorrow", correct: false}, 
+        ]
+    },
+    {
+        question: "On average how far away is the moon from the earth in miles?",
+        answers: [ 
+            {text: "102,500", correct: false},
+            {text: "330,090", correct: false},
+            {text: "157,000", correct:false},
+            {text: "238,000", correct: true}, 
+        ]
+    },
+    {
+        question: "How many notes are there in a musical scale?",
+        answers: [ 
+            {text: "7", correct: true},
+            {text: "4", correct: false},
+            {text: "8", correct:false},
+            {text: "6", correct:false}, 
+        ]
+    },
+    {
+        question: "Which planet has the most moons?",
+        answers: [ 
+            {text: "Saturn", correct: true},
+            {text: "Jupiter", correct: false},
+            {text: "Mars", correct:false},
+            {text: "Venus", correct:false}, 
+        ]
+    },
+    {
+        question: "How many hearts does an octopus have?",
+        answers: [ 
+            {text: "1", correct: false},
+            {text: "5", correct: false},
+            {text: "3", correct:true},
+            {text: "2", correct:false}, 
+        ]
+    },
+    {
+        question: "What is Apple's highest selling Iphone model?",
+        answers: [ 
+            {text: "Iphone X", correct: false},
+            {text: "Iphone 13", correct: false},
+            {text: "Iphone 7", correct:false},
+            {text: "Iphone 6", correct: true}, 
+        ]
+    },
+    {
+        question: "Elon Musk is the CEO of which global brand?",
+        answers: [ 
+            {text: "Microsoft", correct: false},
+            {text: "Facebook", correct: false},
+            {text: "Apple", correct:false},
+            {text: "Tesla", correct: true}, 
+        ]
+    },
+    {
+        question: "What is the highest grossing Marvel movie without the word 'Avengers' in the title?",
+        answers: [ 
+            {text: "Black Panther", correct: true},
+            {text: "Iron Man", correct: false},
+            {text: "Hulk", correct:false},
+            {text: "Captain America", correct:false}, 
+        ]
+    },
+    {
+        question: "What is the largest animal on the planet?",
+        answers: [ 
+            {text: "Elephant", correct: false},
+            {text: "T-Rex", correct: false},
+            {text: "Blue Whale", correct: true},
+            {text: "Great White Shark", correct:false}, 
+        ]
+    },
+    {
+        question: "How high is Mount Everest in metres?",
+        answers: [ 
+            {text: "7,565", correct: false},
+            {text: "8,848", correct: true},
+            {text: "9,256", correct:false},
+            {text: "7,896", correct:false}, 
+        ]
+    },
+    {
+        question: "How many bones are there in the average adult human body?",
+        answers: [ 
+            {text: "206", correct: true},
+            {text: "185", correct: false},
+            {text: "199", correct:false},
+            {text: "211", correct:false}, 
+        ]
+    },
+    {
+        question: "What year did Britain join the European Union?",
+        answers: [ 
+            {text: "1985", correct: false},
+            {text: "1970", correct: false},
+            {text: "1982", correct:false},
+            {text: "1973", correct: true}, 
+        ]
+    },
+    {
+        question: "In which state was former US President Barack Obama born?",
+        answers: [ 
+            {text: "New-York", correct: false},
+            {text: "Hawaii", correct: true},
+            {text: "California", correct:false},
+            {text: "Illionis", correct:false}, 
+        ]
+    },
+    {
+        question: "Botany is the study of what life form?",
+        answers: [ 
+            {text: "Animals", correct: false},
+            {text: "Plants", correct: true},
+            {text: "Humans", correct:false},
+            {text: "Insects", correct:false}, 
+        ]
+    },
+    {
+        question: "What is the busiest airport in Britain called?",
+        answers: [ 
+            {text: "Gatwick", correct: false},
+            {text: "Stanstead", correct: false},
+            {text: "Heathrow", correct: true},
+            {text: "Luton", correct:false}, 
+        ]
+    },
+    {
+        question: "What is the name of one of the longest rivers in the world?",
+        answers: [ 
+            {text: "Nile", correct: false},
+            {text: "Amazon", correct: true},
+            {text: "Thames", correct:false},
+            {text: "Mississippi", correct:false}, 
+        ]
+    },
+    {
+        question: "What number is a baker's dozen?",
+        answers: [ 
+            {text: "13", correct: true},
+            {text: "12", correct: false},
+            {text: "11", correct:false},
+            {text: "14", correct:false}, 
+        ]
+    },
+    {
+        question: "How many elements are in the periodic table?",
+        answers: [ 
+            {text: "97", correct: false},
+            {text: "109", correct: false},
+            {text: "124", correct:false},
+            {text: "118", correct: true}, 
+        ]
+    },
+    {
+        question: "Where is the smallest bone in the human body located?",
+        answers: [ 
+            {text: "Finger", correct: false},
+            {text: "Ear", correct: true},
+            {text: "Toe", correct:false},
+            {text: "Nose", correct:false}, 
+        ]
+    },
+]
